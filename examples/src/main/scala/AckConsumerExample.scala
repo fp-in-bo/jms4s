e1981280020f5f7a07c9ca050dad111aa8cff7ba
Alessandro Zoffoli <alessandro.zoffoli@gmail.com>
2020-04-15 11:51:41 +0200

    Add first example for ack consumer

--- examples/src/main/scala/AckConsumerExample.scala
+++ examples/src/main/scala/AckConsumerExample.scala
@@ -1,29 +1,3 @@
----
-layout: docs
-title:  "Acknowledger Consumer"
----
-
-# Acknowledger Consumer
-
-An `AcknowledgerConsumer` is a consumer which let the client decide whether confirm (a.k.a. ack) or reject (a.k.a. nack) a message after its reception.
-Its only operations is:
-
-```scala
-def handle(f: JmsMessage[F] => F[AckAction[F]]): F[Unit]
-```
-
-This is where the user of the api can specify its business logic, which can be any effectful operation.
-
-What `handle` expects is an `AckAction[F]`, which can be either:
-- an `AckAction.ack`, which will confirm the message
-- an `AckAction.noAck`, which will basically do nothing
-- an `AckAction.send` in all its forms, which can be used to send 1 or multiple messages to 1 or multiple destinations
-
-The consumer can be configured specifying a `concurrencyLevel`, which is used internally to scale the operations (receive and then process up to `concurrencyLevel`).
-
-## A complete example
-
-```scala
 import cats.effect.{ ExitCode, IO, IOApp, Resource }
 import cats.implicits._
 import jms4s.JmsAcknowledgerConsumer.AckAction
@@ -62,4 +36,3 @@ class AckConsumerExample extends IOApp {
     }.as(ExitCode.Success))
   }
 }
-```
