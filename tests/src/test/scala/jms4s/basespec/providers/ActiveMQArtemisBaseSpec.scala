185167f30e90be06f796df1cc279b52ae1a900e7
Alessandro Zoffoli <alessandro.zoffoli@gmail.com>
2020-04-20 08:31:45 +0200

    Fix test flakiness by specifying distinct client ids

--- tests/src/test/scala/jms4s/basespec/providers/ActiveMQArtemisBaseSpec.scala
+++ tests/src/test/scala/jms4s/basespec/providers/ActiveMQArtemisBaseSpec.scala
@@ -7,21 +7,22 @@ import jms4s.activemq.activeMQ._
 import jms4s.basespec.Jms4sBaseSpec
 import jms4s.jms.JmsContext
 
+import scala.util.Random
+
 trait ActiveMQArtemisBaseSpec extends Jms4sBaseSpec {
 
   override def contextRes(implicit cs: ContextShift[IO]): Resource[IO, JmsContext[IO]] =
-    Blocker
-      .apply[IO]
-      .flatMap(blocker =>
-        activeMQ.makeContext[IO](
-          Config(
-            endpoints = NonEmptyList.one(Endpoint("localhost", 61616)),
-            username = Some(Username("admin")),
-            password = Some(Password("passw0rd")),
-            clientId = ClientId("jms-specs")
-          ),
-          blocker
-        )
-      )
-
+    for {
+      blocker <- Blocker.apply[IO]
+      rnd     <- Resource.liftF(IO(Random.nextInt))
+      ctx <- activeMQ.makeContext[IO](
+              Config(
+                endpoints = NonEmptyList.one(Endpoint("localhost", 61616)),
+                username = Some(Username("admin")),
+                password = Some(Password("passw0rd")),
+                clientId = ClientId("jms-specs" + rnd)
+              ),
+              blocker
+            )
+    } yield ctx
 }
