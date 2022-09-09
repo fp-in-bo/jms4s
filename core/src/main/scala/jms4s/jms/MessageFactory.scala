baaf2c2b6bade936d7ccf4919c128840d37268b7
faustin0 <dinatalefausto@gmail.com>
2022-09-09 17:47:56 +0200

    add message cloning api and tests

--- core/src/main/scala/jms4s/jms/MessageFactory.scala
+++ core/src/main/scala/jms4s/jms/MessageFactory.scala
@@ -21,10 +21,58 @@
 
 package jms4s.jms
 
+import cats.syntax.all._
+import cats.{ Applicative, MonadThrow }
+import jms4s.jms.JmsDestination.{ JmsQueue, JmsTopic }
 import jms4s.jms.JmsMessage.JmsTextMessage
+
+import javax.jms.{ Queue, Topic }
+import scala.util.{ Failure, Try }
 
 class MessageFactory[F[_]](private val context: JmsContext[F]) extends AnyVal {
   def makeTextMessage(value: String): F[JmsTextMessage] = context.createTextMessage(value)
+
+  def cloneMessageF(original: JmsTextMessage)(implicit mt: MonadThrow[F]): F[JmsTextMessage] =
+    attemptCloneMessage(original).flatMap(_.liftTo[F])
+
+  def attemptCloneMessage(original: JmsTextMessage)(implicit a: Applicative[F]): F[Either[Throwable, JmsTextMessage]] =
+    original.getText
+      .traverse(makeTextMessage)
+      .map { x =>
+        for {
+          copied <- x
+          _      <- copyMessageHeaders(original, copied)
+        } yield copied
+      }
+      .map(_.toEither)
+
+  private def copyMessageHeaders(message: JmsMessage, newMessage: JmsMessage): Try[Unit] =
+    for {
+      _ <- message.getJMSMessageId.traverse_(newMessage.setJMSMessageID)
+      _ <- message.getJMSTimestamp.traverse_(newMessage.setJMSTimestamp)
+      _ <- message.getJMSCorrelationId.traverse_(newMessage.setJMSCorrelationId)
+      _ <- message.getJMSReplyTo.traverse_ {
+            case queue: Queue => newMessage.setJMSReplyTo(new JmsQueue(queue))
+            case topic: Topic => newMessage.setJMSReplyTo(new JmsTopic(topic))
+            case _            => Failure(new RuntimeException("Unsupported destination"))
+
+          }
+      _ <- message.getJMSDestination.traverse_ {
+            case queue: Queue => newMessage.setJMSDestination(new JmsQueue(queue))
+            case topic: Topic => newMessage.setJMSDestination(new JmsTopic(topic))
+            case _            => Failure(new RuntimeException("Unsupported destination"))
+
+          }
+      _ <- message.getJMSDeliveryMode.traverse_(newMessage.setJMSDeliveryMode)
+      _ <- message.getJMSRedelivered.traverse_(newMessage.setJMSRedelivered)
+      _ <- message.getJMSType.traverse_(newMessage.setJMSType)
+      _ <- message.getJMSExpiration.traverse_(newMessage.setJMSExpiration)
+      _ <- message.getJMSPriority.traverse_(newMessage.setJMSPriority)
+      _ <- message.properties.traverse_ { props =>
+            props.toList.traverse_ { case (k, v) => newMessage.setObjectProperty(k, v) }
+          }
+    } yield ()
+
 }
 
 object MessageFactory {
